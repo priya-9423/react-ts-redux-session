d2703983dec88d675d82bff73b052b261e5c120d
priya-9423 <gatpriyanka1293@gmail.com>
2019-12-02 13:05:45 +0530

    2 files modified

--- reducers/FilterConnector.tsx
+++ reducers/FilterConnector.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 import FilterPanel from "./Components/FilterPanel";
 
 function FilterContainer(props) {
+  return(
 <div>
 <FilterPanel onClick-{filterName => props.dispatch({ type: "SET_FILTER", visibility: filterName })}/>
-</div>
+</div>)
 }
  
  export default connect()(FilterContainer);
--- reducers/index.tsx
+++ reducers/index.tsx
@@ -1,2 +1,7 @@
 import { createStore, combineReducers } from "redux";
 import todos from './TodoReducer'
+import filters from './FilterReducer';
+
+let reducer = combineReducers({todos,filters})
+
+export default(reducer)
